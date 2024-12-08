c975b3623b510aa38673eeeee5f9f4cc39d5df91
Konnov_dd <d.konnov@dt1520.ru>
2024-12-08 17:16:42 +0300

    migrations

--- pabloSite/appTest/models.py
+++ pabloSite/appTest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class AppTest(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField(blank=True) # Этот параметр разрешает null.
+    time_create = models.DateTimeField(auto_now_add=True) # Автоматически заполняет поле при создании дефолтным значением, здесь это текущее время.
+    time_update = models.DateTimeField(auto_now=True) # Будет менять поле каждый раз когда меняется эта запись в бд
+    is_published = models.BooleanField(default=True)
+    id = models.IntegerField(primary_key=True)
